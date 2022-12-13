efa98780ac38764c7f1491c4205ac223afee8547
telimbangs <telimbangs98@gmai.com>
2022-12-12 22:20:43 -0600

    variable.py

---
+++ variable.py
@@ -0,0 +1,24 @@
+x=10
+print(x)
+type(x)
+print(type(x)) #printing out the data type of vriable x
+y=10.7
+print(type(y)) #printing out the data type of variable y
+n="theli"
+print(n)
+print(type(n))
+#x=10
+#y=20
+#z=30
+(x,y,z)=10,20,30
+print(x,y,z)
+p=[5,10,20,30,40]
+print(p)
+print(type(p))
+print(p[2])
+print(p[3])
+d=["dog","cat","pig","lion","tiger","goat"]
+print(d)
+print(type(d))
+print(d[2])
+print(d[5])
