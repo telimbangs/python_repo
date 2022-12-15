3ce8ac1c6cd0becae2b2755c9437b2f4ffcd0ff6
telimbangs <telimbangs98@gmai.com>
2022-12-14 21:51:47 -0600

    python script to add two variables dynamically

---
+++ add_variable.py
@@ -0,0 +1,28 @@
+#store input numbers from the user
+a=input("emter the first number: ")
+b=input("enter the second number: ")
+
+#add the numbears
+sum=float(a) + float(b)
+#print results
+print("the sum of {0} and {1} is {2}".format(a,b,sum))
+
+#subtract the numbers
+
+sub=float(a) - float(b)
+#print results
+print("the sub of {0} and {1} is {2}".format(a,b,sub))
+
+#multiply the numbers
+
+mult=float(a) * float(b)
+#print results
+
+print("the minus of {0} and {1} is {2}".format(a,b,mult))
+
+#divide the numbers
+
+div=float(a) / float(b)
+#print resuls
+
+print("the div of {0} and {1} is {2}".format(a,b,div))
